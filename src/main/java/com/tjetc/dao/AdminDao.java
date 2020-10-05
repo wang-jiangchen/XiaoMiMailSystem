6dcb9f806f272ea4a17e345970a43d9cf64aa95f
yaohu_lzq <2513944974@qq.com>
2020-10-05 10:35:18 +0800

    6

--- src/main/java/com/tjetc/dao/AdminDao.java
+++ src/main/java/com/tjetc/dao/AdminDao.java
@@ -1,4 +1,10 @@
 package com.tjetc.dao;
 
+import com.tjetc.domain.Admin;
+import com.tjetc.domain.Page;
+
 public interface AdminDao {
+    Admin selectAdminByNameAndPassword(String name,String password);
+
+    Page<Admin> selectByPage(int pageNum,int pageSize);
 }
