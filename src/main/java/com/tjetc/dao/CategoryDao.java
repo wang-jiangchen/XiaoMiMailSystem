2d543b8bb68ff0f2d94c342b5ecb39d9858fc414
wjc <1525965038@qq.com>
2020-10-05 10:44:02 +0800

    修改

--- src/main/java/com/tjetc/dao/CategoryDao.java
+++ src/main/java/com/tjetc/dao/CategoryDao.java
@@ -1,4 +1,19 @@
 package com.tjetc.dao;
 
+import com.tjetc.domain.Category;
+import com.tjetc.domain.Page;
+
 public interface CategoryDao {
+    //增加一个分类
+    int insert(Category category);
+    //根据id删除一个分类
+    int delete(int id);
+    //更新分类
+    int update(Category category);
+    //根据分类id查询
+    Category selectById(int id);
+    //根据分类名称模糊查询
+    Page<Category> selectByNameLike(String name);
+    //分页查询所有员工的信息
+    Page<Category> selectByPage(int pageNum,int pageSize);
 }
--- src/main/java/com/tjetc/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/tjetc/dao/impl/CategoryDaoImpl.java
@@ -1,6 +1,52 @@
 package com.tjetc.dao.impl;
 
 import com.tjetc.dao.CategoryDao;
+import com.tjetc.domain.Category;
+import com.tjetc.domain.Page;
+import com.tjetc.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CategoryDaoImpl implements CategoryDao {
+    @Override
+    public int insert(Category category) {
+        return 0;
+    }
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+    @Override
+    public int update(Category category) {
+        return 0;
+    }
+    @Override
+    public Category selectById(int id) {
+        return null;
+    }
+    @Override
+    public Page<Category> selectByNameLike(String name) {
+        return null;
+    }
+    @Override
+    public Page<Category> selectByPage(int pageNum, int pageSize) {
+        return null;
+    }
+    private int count(){
+        Connection conn = DBUtil.getConnection();
+        PreparedStatement pstmt = DBUtil.getPstmt(conn, "select count(1) from category");
+        ResultSet rs = DBUtil.query(pstmt);
+        try {
+            if (rs.next()){
+                return rs.getInt("id");
+            }
+            return 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return 0;
+        }
+    }
 }
